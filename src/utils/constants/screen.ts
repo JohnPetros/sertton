1be8083b59f44f0493844e70263accea6a01f4ae
João Pedro <joaopedro.nc@outlook.com>
2024-02-18 16:51:45 -0300

    ✨ feat (utils): add SCREEN constant

---
+++ src/utils/constants/screen.ts
@@ -0,0 +1,11 @@
+import { Dimensions } from 'react-native'
+import { TAB_BAR_HEIGHT } from './tab-bar-height'
+
+const { width, height } = Dimensions.get('window')
+
+export const SCREEN = {
+  width,
+  height,
+  paddingX: 24,
+  homePaddingBottom: TAB_BAR_HEIGHT * 2,
+}
