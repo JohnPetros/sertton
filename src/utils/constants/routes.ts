040b3681830c7a4561faeb5b0bd684b16b80a47c
João Pedro <joaopedro.nc@outlook.com>
2024-02-18 16:15:35 -0300

    ✨ feat (utils): add ROUTES constant

---
+++ src/utils/constants/routes.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  products: '/(drawer)/(tabs)/products',
+  home: '/(drawer)/(tabs)/home',
+  profile: '/(stack)/profile',
+  privacyPolicy: '/(stack)/privacy-policy',
+  termsAndConditions: '/(stack)/terms-and-conditions',
+  about: '/(stack)/about',
+  cart: '/(drawer)/(tabs)/cart',
+  orders: '/(drawer)/(tabs)/orders',
+  productDetails: '/(drawer)/(tabs)/product-details',
+  splash: '/(stack)/splash',
+}
