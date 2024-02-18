f1b6a1a3b03c2f7dac5eef4323be577eb8811cfe
João Pedro <joaopedro.nc@outlook.com>
2024-02-18 16:49:47 -0300

    ✨ feat (components): add shared Button  component

---
+++ src/components/shared/Button/index.tsx
@@ -0,0 +1,35 @@
+import { Button as TamaguiButton, styled } from 'tamagui'
+
+export const Button = styled(TamaguiButton, {
+  height: 44,
+  color: '$white',
+  fontSize: 14,
+  borderRadius: 4,
+  borderWidth: 1,
+
+  variants: {
+    background: {
+      primary: {
+        bg: '$blue400',
+        color: '$white',
+      },
+      secondary: {
+        bg: '$gray400',
+        color: '$white',
+      },
+      outline: {
+        bg: '$colorTransparent',
+        borderColor: '$blue400',
+        color: '$blue400',
+      },
+      transparent: {
+        bg: '$colorTransparent',
+        color: '$gray800',
+      },
+    },
+  } as const,
+
+  defaultVariants: {
+    background: 'primary',
+  },
+})
