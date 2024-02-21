a946c287b1839113049923c8f0c6ff60f2d719f5
João Pedro <joaopedro.nc@outlook.com>
2024-02-21 20:21:09 -0300

    🔧 chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+// jest.config.js
+/** @type {import('jest').Config} */
+
+const config = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+      "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+    ],
+  coveragePathIgnorePatterns: ['node_modules'],
+  testPathIgnorePatterns: [
+    '<rootDir>/src/_tests_/mocks/',
+    '<rootDir>/src/_tests_/customs/',
+    '<rootDir>/src/_tests_/config/',
+    '<rootDir>/src/_tests_/coverage/',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+}
+
+module.exports = config
