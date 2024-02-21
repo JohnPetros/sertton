7843aedbd9767840aa1ad8d73d4db3394ed58043
João Pedro <joaopedro.nc@outlook.com>
2024-02-21 20:28:39 -0300

    ✨ feat (providers): add ToastProvider

---
+++ src/providers/ToastProvider.tsx
@@ -0,0 +1,29 @@
+import { Truck } from 'phosphor-react-native'
+import { ReactNode } from 'react'
+
+import { ToastProvider as Provider } from 'react-native-toast-notifications'
+
+import { getTokens } from 'tamagui'
+
+interface ToastProviderProps {
+  children: ReactNode
+}
+
+export function ToastProvider({ children }: ToastProviderProps) {
+  return (
+    <Provider
+      placement='bottom'
+      duration={5000}
+      animationType='slide-in'
+      successColor={getTokens().color.blue500.val}
+      dangerColor={getTokens().color.red500.val}
+      warningColor={getTokens().color.yellow500.val}
+      icon={<Truck color={getTokens().color.white.val} />}
+      textStyle={{ fontSize: 16, paddingHorizontal: 16 }}
+      offsetBottom={80}
+      swipeEnabled={true}
+    >
+      {children}
+    </Provider>
+  )
+}
