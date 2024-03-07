604e52ddd1e6221a4d7e780671d23723cac965c6
João Pedro <joaopedro.nc@outlook.com>
2024-03-07 17:38:56 -0300

    🧪 test: add useSharedValueMock

---
+++ src/_tests_/mocks/libs/react-reanimated/useSharedValueMock.ts
@@ -0,0 +1,17 @@
+import { SharedValue } from "react-native-reanimated"
+
+export function useSharedValueMock(initialValue: number) {
+  let value = initialValue
+
+  const sharedValue = {
+    set value(newValue: number) {
+      value = newValue
+    },
+
+    get value() {
+      return value
+    }
+  }
+
+  return sharedValue as SharedValue<number>
+}
