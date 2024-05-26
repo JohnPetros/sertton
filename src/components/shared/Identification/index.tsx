f2132f5fedbfc9eb7b848d2b31841336278f7523
João Pedro <joaopedro.nc@outlook.com>
2024-05-25 22:22:41 -0300

    💄 styles: whiten Indentification texts

--- src/components/shared/Identification/index.tsx
+++ src/components/shared/Identification/index.tsx
@@ -9,13 +9,13 @@ export function Indentification() {
   return (
     <YStack gap={8} alignItems='center'>
       <Separator vertical={false} w='90%' bg='$gray200' />
-      <Text color='$gray200' fontSize={12}>
+      <Text color='$gray50' fontSize={12}>
         Rua Tomatssu Iawasse 233 - Vila Nova Bonsucesso
       </Text>
-      <Text color='$gray200' fontSize={12}>
+      <Text color='$gray50' fontSize={12}>
         © {date.format(new Date(), 'YYYY')} Sertton Brasil Distribuidora Ltda
       </Text>
-      <Text color='$gray200' fontSize={12}>
+      <Text color='$gray50' fontSize={12}>
         CNPJ: 33.805.461/0001-90
       </Text>
     </YStack>
