84af7401a799dcf648e6939e06bc3e56894ff6b7
João Pedro <joaopedro.nc@outlook.com>
2025-02-02 19:43:08 -0300

    ♨️ perf: remove logs from useNetworkChecker hook

--- src/components/layout/network-checker/use-network-checker.ts
+++ src/components/layout/network-checker/use-network-checker.ts
@@ -4,8 +4,6 @@ import { useEffect, useState } from 'react'
 export function useNetworkChecker() {
   const [isNetworkConnected, setIsNetworkConnected] = useState(true)
 
-  console.log(isNetworkConnected)
-
   useEffect(() => {
     NetInfo.addEventListener((netState) => {
       setIsNetworkConnected(Boolean(netState.isConnected))
