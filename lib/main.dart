50e77ea36338c66ea6bd297bfd2bdc7a8e8568fc
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-19 15:35:12 -0300

    🖥️ ui: create main application entry point with Riverpod integration and ShadcnApp configuration

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sertton/router.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return ShadcnApp.router(
+      title: 'Sertton',
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+      theme: ThemeData(colorScheme: ColorSchemes.lightBlue, radius: 0.5),
+      scaling: const AdaptiveScaling(1.05),
+    );
+  }
+}
