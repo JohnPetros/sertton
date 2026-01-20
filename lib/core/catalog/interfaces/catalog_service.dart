2eb60224cc2ae36273d39749761ad981cbff861b
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-20 07:53:45 -0300

    📶 rest: update CatalogService to use PaginationResponse and add fetchCategories method for enhanced data retrieval

--- lib/core/catalog/interfaces/catalog_service.dart
+++ lib/core/catalog/interfaces/catalog_service.dart
@@ -1,12 +1,14 @@
+import 'package:sertton/core/catalog/dtos/category_dto.dart';
 import 'package:sertton/core/catalog/dtos/collection_dto.dart';
 import 'package:sertton/core/catalog/dtos/product_dto.dart';
-import 'package:sertton/core/global/responses/cursor_pagination_response.dart';
+import 'package:sertton/core/global/responses/pagination_response.dart';
 import 'package:sertton/core/global/responses/rest_response.dart';
 
 abstract class CatalogService {
-  Future<RestResponse<CursorPaginationResponse<ProductDto>>> fetchProducts();
+  Future<RestResponse<PaginationResponse<ProductDto>>> fetchProducts();
   Future<RestResponse<ProductDto>> fetchProduct();
   Future<RestResponse<List<CollectionDto>>> fetchCollections();
+  Future<RestResponse<List<CategoryDto>>> fetchCategories();
   Future<RestResponse<List<ProductDto>>> fetchProductsByCollection(
     String collectionId,
   );
