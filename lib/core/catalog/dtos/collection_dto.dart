6bcb0968ad09d95efd528a194965496c4be0bf5e
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-20 17:20:09 -0300

    📶 rest: update CollectionDto to remove products field, implement YampiCollectionMapper for DTO conversion, and enhance YampiCatalogService with fetchCollections method for improved data retrieval

--- lib/core/catalog/dtos/collection_dto.dart
+++ lib/core/catalog/dtos/collection_dto.dart
@@ -1,9 +1,6 @@
-import 'package:sertton/core/catalog/dtos/product_dto.dart';
-
 class CollectionDto {
   final String id;
   final String name;
-  final List<ProductDto> products;
 
-  CollectionDto({required this.id, required this.name, required this.products});
+  CollectionDto({required this.id, required this.name});
 }
--- lib/rest/services.dart
+++ lib/rest/services.dart
@@ -1,12 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:sertton/core/catalog/interfaces/catalog_service.dart';
+
+import 'package:sertton/core/marketing/interfaces/marketing_service.dart';
 import 'package:sertton/drivers/env-driver/index.dart';
 import 'package:sertton/rest/rest_client.dart';
 import 'package:sertton/rest/yampi/services/yampi_catalog_service.dart';
+import 'package:sertton/rest/yampi/services/yampi_marketing_service.dart';
 
 final catalogServiceProvider = Provider<CatalogService>((ref) {
   final restClient = ref.read(restClientProvider);
   final envDriver = ref.read(envDriverProvider);
   return YampiCatalogService(restClient, envDriver);
+});
+
+final marketingServiceProvider = Provider<MarketingService>((ref) {
+  final restClient = ref.read(restClientProvider);
+  final envDriver = ref.read(envDriverProvider);
+  return YampiMarketingService(restClient, envDriver);
 });
