ded0480e80d659d47424eb69ff36db43ba86df04
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-22 15:35:27 -0300

    📶 rest: implement CheckoutService interface and YampiCheckoutService for handling checkout links

--- lib/rest/services.dart
+++ lib/rest/services.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sertton/core/catalog/interfaces/catalog_service.dart';
 
+import 'package:sertton/core/catalog/interfaces/catalog_service.dart';
+import 'package:sertton/core/checkout/interfaces/checkout_service.dart';
 import 'package:sertton/core/marketing/interfaces/marketing_service.dart';
+
 import 'package:sertton/drivers/env-driver/index.dart';
 import 'package:sertton/rest/rest_client.dart';
 import 'package:sertton/rest/yampi/services/yampi_catalog_service.dart';
+import 'package:sertton/rest/yampi/services/yampi_checkout_service.dart';
 import 'package:sertton/rest/yampi/services/yampi_marketing_service.dart';
 
 final catalogServiceProvider = Provider<CatalogService>((ref) {
@@ -17,4 +20,10 @@ final marketingServiceProvider = Provider<MarketingService>((ref) {
   final restClient = ref.read(restClientProvider);
   final envDriver = ref.read(envDriverProvider);
   return YampiMarketingService(restClient, envDriver);
+});
+
+final checkoutServiceProvider = Provider<CheckoutService>((ref) {
+  final restClient = ref.read(restClientProvider);
+  final envDriver = ref.read(envDriverProvider);
+  return YampiCheckoutService(restClient, envDriver);
 });
