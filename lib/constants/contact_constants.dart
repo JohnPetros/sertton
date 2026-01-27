d8c5f5fbc0117ff3a3e1b00d90f47419a825bc3d
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-27 01:16:14 -0300

    🪨 constants: add Contacts class for WhatsApp, landline, and email information

---
+++ lib/constants/contact_constants.dart
@@ -0,0 +1,14 @@
+class Contacts {
+  static const whatsappNumber = '5512988233818';
+  static const whatsappUrl =
+      'whatsapp://send?phone=$whatsappNumber&text=Olá, gostaria de saber mais sobre a Sertton.';
+  static const whatsappLabel = '(12) 988233818';
+
+  static const landlineNumber = '551149682964';
+  static const landlineUrl = 'tel:$landlineNumber';
+  static const landlineLabel = '(11) 1149682964';
+
+  static const email = 'falecom@sertton.ind.br';
+  static const emailUrl = 'mailto:$email';
+  static const emailLabel = 'falecom@sertton.ind.br';
+}
