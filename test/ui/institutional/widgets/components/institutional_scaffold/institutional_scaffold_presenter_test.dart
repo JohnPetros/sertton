9c296b22e97bfa367dc12ea5085bbf4fefdde64e
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-28 10:53:49 -0300

    🧪 test: add unit tests for InstitutionalScaffoldPresenter and InstitutionalScaffoldView to verify navigation behavior and UI rendering

---
+++ test/ui/institutional/widgets/components/institutional_scaffold/institutional_scaffold_presenter_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:sertton/core/global/interfaces/navigation_driver.dart';
+import 'package:sertton/ui/institutional/widgets/components/institutional_scaffold/institutional_scaffold_presenter.dart';
+
+class MockNavigationDriver extends Mock implements NavigationDriver {}
+
+void main() {
+  late MockNavigationDriver navigationDriver;
+  late InstitutionalScaffoldPresenter presenter;
+
+  setUp(() {
+    navigationDriver = MockNavigationDriver();
+    presenter = InstitutionalScaffoldPresenter(navigationDriver);
+  });
+
+  group('InstitutionalScaffoldPresenter', () {
+    test('should go back when canGoBack is true', () {
+      when(() => navigationDriver.canGoBack()).thenReturn(true);
+
+      presenter.onBack();
+
+      verify(() => navigationDriver.canGoBack()).called(1);
+      verify(() => navigationDriver.back()).called(1);
+      verifyNever(() => navigationDriver.go(any()));
+    });
+
+    test('should go to root when canGoBack is false', () {
+      when(() => navigationDriver.canGoBack()).thenReturn(false);
+
+      presenter.onBack();
+
+      verify(() => navigationDriver.canGoBack()).called(1);
+      verify(() => navigationDriver.go('/')).called(1);
+      verifyNever(() => navigationDriver.back());
+    });
+  });
+}
