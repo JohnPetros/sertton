05ed1d04136c1511b531215daa1d7824cadfd75b
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-02-05 19:34:20 -0300

    📦 deps: bump version to 1.0.6 in pubspec.yaml

--- lib/ui/global/widgets/screens/home/home_screen_view.dart
+++ lib/ui/global/widgets/screens/home/home_screen_view.dart
@@ -1,6 +1,7 @@
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:sertton/drivers/env-driver/index.dart';
 import 'package:sertton/ui/global/widgets/screens/home/leads-capturer-section/index.dart';
 import 'package:sertton/ui/global/widgets/screens/home/marketing-section/index.dart';
 import 'package:sertton/ui/global/widgets/screens/home/footer-section/index.dart';
@@ -10,10 +11,17 @@ class HomeScreenView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final envDriver = ref.read(envDriverProvider);
     return Scaffold(
       child: SingleChildScrollView(
         child: Column(
           children: [
+            Text(
+              "YAMPI_API_URLYAMPI_API_URL: ${envDriver.get('YAMPI_API_URLYAMPI_API_URL')}",
+            ),
+            Text("YAMPI_PURCHASE_URL: ${envDriver.get('YAMPI_PURCHASE_URL')}"),
+            Text("YAMPI_USER_TOKEN: ${envDriver.get('YAMPI_USER_TOKEN')}"),
+            Text("YAMPI_SECRET_KEY: ${envDriver.get('YAMPI_SECRET_KEY')}"),
             MarketingSection(),
             LeadsCapturerSectionView(),
             const HomeFooterSection(),
